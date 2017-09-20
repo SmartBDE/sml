a3c54f1af38236c83c5a8f73f207e8db78bac143
principality <principality@home>
2017-09-21 00:40:38 +0800

    flume http

---
+++ src/test/java/principality/me/springtest/model/FlumeEvent.java
@@ -0,0 +1,12 @@
+package principality.me.springtest.model;
+
+import java.util.HashMap;
+
+public class FlumeEvent {
+    public HashMap<String, String> headers;
+    public String body;
+
+    public FlumeEvent() {
+        headers = new HashMap<>();
+    }
+}
