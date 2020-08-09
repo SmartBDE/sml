173a1b56084013f55997429f95f846f032bbe513
principality <principality@home>
2020-08-09 23:24:06 +0800

    0809 修正依赖及版本2

--- storage/src/main/java/me/smartbde/sml/storage/IStorage.java
+++ storage/src/main/java/me/smartbde/sml/storage/IStorage.java
@@ -1,4 +1,24 @@
 package me.smartbde.sml.storage;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+
 public interface IStorage {
+    public void init(String repository);
+
+    public void create(String path);
+
+    public void drop(String path);
+
+    public Dataset<Row> read(String name);
+
+    /**
+     * 批量写入，采用truncate的模式
+     *
+     * @param name 目标的名字
+     * @param ds 数据
+     */
+    public void write(String name, Dataset<Row> ds);
+
+    public void delete(String name);
 }
