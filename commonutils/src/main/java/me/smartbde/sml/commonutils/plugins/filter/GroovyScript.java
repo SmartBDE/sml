889dd4571c918e705d2673e30b3736c8e6a6a042
principality <principality@home>
2020-09-30 00:53:35 +0800

    0930 GroovyScript实现

--- commonutils/src/main/java/me/smartbde/sml/commonutils/plugins/filter/GroovyScript.java
+++ commonutils/src/main/java/me/smartbde/sml/commonutils/plugins/filter/GroovyScript.java
@@ -6,17 +6,23 @@ import javafx.util.Pair;
 import me.smartbde.sml.commonutils.AbstractPlugin;
 import me.smartbde.sml.commonutils.IFilter;
 import me.smartbde.sml.commonutils.ISession;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.MapFunction;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.types.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scala.reflect.ClassTag;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * 功能说明：对Dataset<Row>中的信息，用脚本进行处理，通常这发生在Dataset<Row>.map中
@@ -27,33 +33,72 @@ import java.util.Map;
 public class GroovyScript extends AbstractPlugin implements IFilter {
     private GroovyClassLoader groovyClassLoader;
     private Logger logger = LoggerFactory.getLogger(GroovyScript.class);
+    private GroovyObject groovyObject;
 
     @Override
     public Dataset<Row> process(SparkSession spark, Dataset<Row> df, ISession session) {
+        JavaRDD<Row> rdd = df.toJavaRDD();
 
-        Map<Integer, String> map = new HashMap<>();
-        map.put(0, "a");
-        map.put(1, "b");
-        map.put(2, "c");
+        JavaRDD<Row> rdd2 = rdd.map(new Function<Row, Row>() {
+            @Override
+            public Row call(Row row) throws Exception {
+                // 解析row，然后将参数传给script
 
-        try {
-            FileInputStream inputStream = new FileInputStream(properties.get("script"));
-            int length = inputStream.available();
-            byte bytes[] = new byte[length];
-            inputStream.read(bytes);
-            inputStream.close();
-            String content = new String(bytes, StandardCharsets.UTF_8);
-            Class groovyClass = groovyClassLoader.parseClass(content);
+                String schemaFormat = properties.get("inputSchema");
+                StructField[] structFields = parseSchema(schemaFormat);
+                int length = structFields.length;
 
-            GroovyObject groovyObject = (GroovyObject) groovyClass.newInstance();
-            Object object = groovyObject.invokeMethod(properties.get("func"), map);
-        } catch (IOException e) {
-            logger.warn(e.toString());
-        } catch (Exception e) {
-            logger.warn(e.toString());
+                List<Object> args = new ArrayList<>();
+                for (int i = 0; i < length; i++) {
+                    args.add(row.get(i));
+                }
+
+                Object object = groovyObject.invokeMethod(properties.get("func"), args);
+                List<Object> objects = (List<Object>) object;
+
+                // 将object转换为Row
+                Row r = RowFactory.create(objects);
+                return r;
+            }
+        });
+
+        String schemaFormat2 = properties.get("outputSchema");
+        StructField[] structFields2 = parseSchema(schemaFormat2);
+        StructType schema = DataTypes.createStructType(structFields2);
+
+        return spark.createDataFrame(rdd2, schema);
+    }
+
+    // schema的格式：fieldName:type fieldName:type fieldName:type ...
+    private StructField[] parseSchema(String schemaFormat) {
+        final Pattern Space = Pattern.compile(" ");
+        String fields[] = Space.split(schemaFormat);
+
+        StructField[] structFields = new StructField[fields.length];
+        for (int i = 0; i < fields.length; i++) {
+            String[] splits = fields[i].split(":");
+            String field = splits[0];
+            DataType type;
+            switch (splits[1]) {
+                case "int":
+                    type = DataTypes.IntegerType;
+                    break;
+                case "string":
+                    type = DataTypes.StringType;
+                    break;
+                case "binary":
+                    type = DataTypes.BinaryType;
+                    break;
+                case "bool":
+                    type = DataTypes.BooleanType;
+                    break;
+                default:
+                    type = DataTypes.NullType;
+            }
+
+            structFields[i] = new StructField(field, type, false, Metadata.empty());
         }
-
-        return null;
+        return structFields;
     }
 
     @Override
@@ -64,7 +109,8 @@ public class GroovyScript extends AbstractPlugin implements IFilter {
 
         if (properties.get("script") != null
                 && properties.get("func") != null
-                && properties.get("schema") != null) {
+                && properties.get("ischema") != null
+                && properties.get("oschema") != null) {
             return new Pair<>(true, "");
         }
 
@@ -78,8 +124,6 @@ public class GroovyScript extends AbstractPlugin implements IFilter {
 
     @Override
     public boolean prepare(SparkSession spark) {
-
-
         if (properties != null && checkConfig().getKey()) {
             try {
 //                String separator = System.getProperty("file.separator");
@@ -87,8 +131,24 @@ public class GroovyScript extends AbstractPlugin implements IFilter {
 //                int index = path.lastIndexOf(separator);
 //                String root = path.substring(0, index);
 //                String file = path.substring(index+1, path.length());
-                ClassLoader cl = new GroovyScript().getClass().getClassLoader();
+                ClassLoader cl = GroovyScript.class.getClassLoader();
                 groovyClassLoader = new GroovyClassLoader(cl);
+                try {
+                    FileInputStream inputStream = new FileInputStream(properties.get("script"));
+                    int length = inputStream.available();
+                    byte[] bytes = new byte[length];
+                    inputStream.read(bytes);
+                    inputStream.close();
+                    // 文件编码必须为utf-8
+                    String content = new String(bytes, StandardCharsets.UTF_8);
+                    // 装载并编译
+                    Class groovyClass = groovyClassLoader.parseClass(content);
+                    groovyObject = (GroovyObject) groovyClass.newInstance();
+                } catch (IOException e) {
+                    logger.warn(e.toString());
+                } catch (Exception e) {
+                    logger.info(e.toString());
+                }
             } catch (Exception e) {
                 logger.info(e.toString());
             }
