5d9961e667f2713c2a150e73edfb8a5fd3b82d55
principality <principality@home>
2021-06-16 02:02:30 +0800

    0616 monitor:add rest api

---
+++ monitor/monitor-server/src/main/java/sml/smartdbe/me/rest/HelloService.java
@@ -0,0 +1,15 @@
+package sml.smartdbe.me.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("hello")
+public class HelloService {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello(){
+        return "hello jersey";
+    }
+}
